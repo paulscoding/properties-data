3d8d710ecea03d2c419cb2c5385839c5304a38e9
Fusion569 <paul.thibau74140@gmail.com>
2022-05-29 15:33:45 +0200

    Add javadoc

--- src/fr/fusion569/propertiesdata/PropertiesData.java
+++ src/fr/fusion569/propertiesdata/PropertiesData.java
@@ -16,10 +16,22 @@ public class PropertiesData {
     private PropertiesData() {
     }
 
+    /**
+     * Get the {@link String} logs prefix.
+     *
+     * @return
+     * the {@link String} logs prefix.
+     */
     public static String getLogsPrefix() {
         return LOGS_PREFIX;
     }
 
+    /**
+     * Set the new {@link String} logs prefix.
+     *
+     * @param logsPrefix
+     * the new {@link String} logs prefix.
+     */
     public static void setLogsPrefix(String logsPrefix) {
         LOGS_PREFIX = logsPrefix;
     }
