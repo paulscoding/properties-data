721cc5a3375d08abe9c618579e0036fdf4707f82
Fusion569 <paul.thibau74140@gmail.com>
2022-06-24 23:13:12 +0200

    Removed main test method

--- src/fr/fusion569/propertiesdata/PropertiesData.java
+++ src/fr/fusion569/propertiesdata/PropertiesData.java
@@ -1,10 +1,5 @@
 package fr.fusion569.propertiesdata;
 
-import fr.fusion569.propertiesdata.files.FileProperties;
-import fr.fusion569.propertiesdata.utils.KeyValueSeparator;
-import fr.fusion569.propertiesdata.utils.StandardDirectoryCreationType;
-import fr.fusion569.propertiesdata.utils.StandardFileCreationType;
-
 /**
  * This class represents the main class.
  */
@@ -39,21 +34,5 @@ public final class PropertiesData {
      */
     public static void setLogsPrefix(String logsPrefix) {
         LOGS_PREFIX = logsPrefix;
-    }
-
-    public static void main(String[] args) {
-        final FileProperties file = new FileProperties(
-                "/Users/Paul/Desktop/",
-                "data",
-                StandardFileCreationType.ONLY_WANTED_FILE,
-                StandardDirectoryCreationType.IGNORE,
-                KeyValueSeparator.DOUBLE_POINTS_AND_SPACE
-        );
-
-        file.create();
-        file.setString("name", "Paul");
-        file.setInteger("age", 14);
-
-        System.out.println(file.contains("able"));
     }
 }
