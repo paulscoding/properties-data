34666a8b6d9b4e577a5d298aba27587554433afe
Fusion569 <paul.thibaut74140@gmail.com>
2022-06-24 17:57:46 +0200

    Removed main test method

--- src/fr/fusion569/propertiesdata/PropertiesData.java
+++ src/fr/fusion569/propertiesdata/PropertiesData.java
@@ -1,10 +1,5 @@
 package fr.fusion569.propertiesdata;
 
-import fr.fusion569.propertiesdata.files.PropertiesFile;
-import fr.fusion569.propertiesdata.utils.KeyValueSeparator;
-import fr.fusion569.propertiesdata.utils.StandardDirectoryCreationType;
-import fr.fusion569.propertiesdata.utils.StandardFileCreationType;
-
 /**
  * This class represents the main class.
  */
@@ -39,27 +34,5 @@ public final class PropertiesData {
      */
     public static void setLogsPrefix(String logsPrefix) {
         LOGS_PREFIX = logsPrefix;
-    }
-
-    public static void main(String[] args) {
-        final PropertiesFile file = new PropertiesFile(
-                "C://Users/Papa/Desktop/",
-                "data",
-                StandardFileCreationType.ONLY_WANTED_FILE,
-                StandardDirectoryCreationType.IGNORE,
-                KeyValueSeparator.DOUBLE_POINTS_AND_SPACE
-        );
-
-        file.create();
-        file.setString("name", "Paul");
-        file.setInteger("age", 14);
-        file.setDouble("height", 1.60);
-        file.setFloat("weight", 57.0f);
-        file.setBoolean("has-gf", false);
-        System.out.println(file.getString("name"));
-        System.out.println(file.getInteger("age"));
-        System.out.println(file.getDouble("height"));
-        System.out.println(file.getFloat("weight"));
-        System.out.println(file.getBoolean("has-gf"));
     }
 }
